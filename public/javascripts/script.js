332c4c6a1693d80abb309aafc49b43da8e0dc9cd
Asif Muhammed <asifasifpsps@gmail.com>
2022-05-30 21:25:27 +0530

    botton nav added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -77,3 +77,28 @@
     
 })(jQuery);
 
+
+// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+mdc.autoInit()
+
+var lists = document.querySelectorAll('.mdc-bottom-navigation__list')
+var activatedClass = 'mdc-bottom-navigation__list-item--activated'
+for (var i = 0, list; list = lists[i]; i++) {
+  list.addEventListener('click', function(event) {
+    var el = event.target
+    while (!el.classList.contains('mdc-bottom-navigation__list-item') && el) {
+      el = el.parentNode
+    }
+    if (el) {
+      var selectRegex = /.*(card-\d).*/;
+      var activatedItem = document.querySelector('.' + event.target.parentElement.parentElement.parentElement.className.replace(selectRegex, '$1') + ' .' + activatedClass)
+      if (activatedItem) {
+        activatedItem.classList.remove(activatedClass)
+      }
+      event.target.classList.add(activatedClass)
+    }
+  })
+}
+
+
