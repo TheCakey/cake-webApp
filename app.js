948defffa46c19659b9869796d9451ce1b5785a2
mohammed yaseen <yyaseen080@gmail.com>
2022-07-26 19:26:42 +0530

    profile page changed something session time increased

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials/'}))
-app.use(session({secret:"key",cookie:{maxAge:120000}}))
+app.use(session({secret:"key",cookie:{maxAge:86400000}}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
