227dc8d169a812c1cb0f036343d07d2f903dde44
Asif Muhammed <asifasifpsps@gmail.com>
2022-07-26 19:25:32 +0530

    products photo dynamically setted

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -5,7 +5,7 @@ var objectId=require('mongodb').ObjectID
 module.exports={
   
     addProduct:(product,callback)=>{
-        console.log('kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk')
+      
         db.get().collection(collection.PRODUCT_COLLECTION).insertOne(product).then((data)=>{
             console.log(data.insertedId);
             callback(data.insertedId)
